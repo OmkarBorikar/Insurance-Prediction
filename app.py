a601846c08a71f2679b296110624af48a47cd5f1
Omkar Borikar <omeeborikar@gmail.com>
2021-11-23 21:05:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+# st.title('Predict Insurance cost')
+
+st.markdown("<h1 style='text-align: center; color: red;'>Predict Insurance cost</h1>", unsafe_allow_html=True)
+pipe_cat = pickle.load(open('pipe_cat.pkl','rb'))
+pipe_xgb = pickle.load(open('pipe_xgb.pkl','rb'))
+pipe_rf = pickle.load(open('pipe_rf.pkl','rb'))
+
+col1,col2 = st.columns(2)
+with col1:
+    f_name = st.text_input('Enter First Name') 
+with col2:
+    l_name = st.text_input('Enter Last Name')
+
+age = st.slider('Select Age',min_value = 18 , max_value=70 , step=1)
+col3,col4,col5 = st.columns(3)
+with col3:
+    sex = st.radio("Select Gender: ", ('male', 'female'))
+
+with col4:
+    smoker = st.radio("Smoker (Yes/No): ", ('yes', 'no'))
+
+with col5:
+    children = st.number_input('Number of Childern',min_value = 0 , max_value = 5 , step=1)
+
+bmi = st.number_input('Enter BMI',min_value = 14.00 , max_value = 55.00,step = 0.1)
+
+region = st.selectbox('Select Region',options=['southwest', 'southeast', 'northwest', 'northeast'])
+
+if st.button('Predict Insurance cost'):
+    input_df = pd.DataFrame({  'age':[age] , 'sex':[sex] , 'bmi' : [bmi] , 'children' : [children] , 'smoker' : [smoker] , 'region' : [region]
+
+    })
+    st.table(input_df)
+    y_pred_cat = pipe_cat.predict(input_df)
+    y_pred_xgb = pipe_xgb.predict(input_df)
+    y_pred_rf = pipe_rf.predict(input_df)
+
+    result = (6*y_pred_cat + 2.5*y_pred_xgb+ 1.5*y_pred_rf)/10
+    st.header('Insurance cost is - %.2f'%(float(result)) + ' $')
